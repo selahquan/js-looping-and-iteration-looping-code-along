36aaf6aefe1772f1183f5de908f509fd4ca7f94d
Josh <josh.handon@gmail.com>
2021-11-11 16:53:46 -0500

    commit tree 2b69db71ce64f8957931343adfc2180ca5190568

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+
+
+function writeCards(arr, eventName) {
+    let thankYouCards = []
+    for (let i=0; i<arr.length; i++){
+        thankYouCards.push(`Thank you, ${arr[i]}, for the wonderful ${eventName} gift!`)
+    }
+    return thankYouCards
+}
+
+function countDown(startNumber){
+    while(startNumber > 0){
+        console.log(startNumber);
+        startNumber -= 1;
+    }
+    console.log(startNumber);
+}
